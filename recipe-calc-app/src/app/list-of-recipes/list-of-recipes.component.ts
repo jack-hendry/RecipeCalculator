0f8b6cf7c82c234c25f5ff936e8874a36a9e7be0
Jack Hendry <Jack.r.Hendry.p@gmail.com>
2022-12-19 17:05:57 -0500

    Added sorting fnaility for recipe names

--- recipe-calc-app/src/app/list-of-recipes/list-of-recipes.component.ts
+++ recipe-calc-app/src/app/list-of-recipes/list-of-recipes.component.ts
@@ -1,6 +1,6 @@
 import { RecipeService } from './shared/recipe.service';
 import { GeneralRecipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
@@ -8,21 +8,50 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   templateUrl: './list-of-recipes.component.html',
   styleUrls: ['./list-of-recipes.component.css'],
 })
-export class ListOfRecipesComponent implements OnInit {
+export class ListOfRecipesComponent implements OnInit, OnChanges {
   listOfRecipes: GeneralRecipe[];
   selectedRecipeBase: GeneralRecipe;
   selectedRecipeID: number;
   selectedRecipeFlag: boolean = false;
+  visibleRecipes: GeneralRecipe[];
+  sortBy: string = '';
 
   constructor(
     private recipeService: RecipeService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router, private cd: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
     this.listOfRecipes = this.recipeService.getListOfRecipes();
+    this.visibleRecipes = this.listOfRecipes.slice(0);
     console.log(this.route);
+    console.log(this.sortBy);
+    console.log(this.visibleRecipes);
+  }
+
+  ngOnChanges(): void {
+
+    if (this.listOfRecipes) {
+      if (this.sortBy === 'ascending') {
+        this.visibleRecipes.sort(this.sortAscending);
+      }
+      else if (this.sortBy === 'descending'){
+        this.visibleRecipes.sort(this.sortDescending)
+      }
+    }
+  }
+
+  sortAscending(r1: GeneralRecipe, r2: GeneralRecipe) {
+
+      if (r1.name > r2.name) return 1;
+      else if (r1.name === r2.name) return 0;
+      else return -1;
+  }
+  sortDescending(r1:GeneralRecipe, r2: GeneralRecipe) {
+    if (r1.name < r2.name) return 1;
+      else if (r1.name === r2.name) return 0;
+      else return -1;
   }
 
   // toggleRecipeFlag(){
